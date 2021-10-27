ce971c9008fa754173cbcd1247bdf3889a0c26f2
PadowYT2 <me@padow.ru>
2021-10-27 09:27:08 +0300

    Update create_release.sh

--- create_release.sh
+++ create_release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ "$1" == "-h" ] | [ -z "$1" ]; then
-  echo "usage: ./create_release.sh <version>"
+  echo "usage: ./create_release.sh"
   exit
 else
   version=$1
 fi
 
-echo " zipping template files for $version"
+echo " zipping template files"
 
-zip -r "Nexus "$version".zip" custom/templates/Nexus/ custom/panel_templates/Default/nexus/ modules/Nexus/ README.html README.md
+zip -r "Nexus.zip" custom/templates/Nexus/ custom/panel_templates/Default/nexus/ modules/Nexus/ README.html README.md
