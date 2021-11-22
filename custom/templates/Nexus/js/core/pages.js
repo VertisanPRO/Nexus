10a1ba93ace6153dbe527200f6e916fa09f1f6d4
BlackBeltPanda <dev@pandacraft.org>
2021-11-22 13:04:10 -0500

    Properly remove profile links in server status for nonexisting profiles

--- custom/templates/Nexus/js/core/pages.js
+++ custom/templates/Nexus/js/core/pages.js
@@ -22,13 +22,22 @@ if (page !== '') {
 						} else {
 							if (serverPlayerList === 1) {
 								if (data.player_list.length > 0) {
-									for (var i = 0; i < data.player_list.length; i++) {
-										players += '<a href="' + URLBuild('profile/' + data.player_list[i].name) + '" data-tooltip="' + data.player_list[i].name + '" data-variation="mini" data-inverted="" data-position="bottom center"><img class="ui mini circular image" src="' + avatarSource.replace('{x}', data.player_list[i].id).replace('{y}', 64) + '" alt="' + data.player_list[i].name + '"></a>';
-									}
-
-									if(data.player_list.length < data.player_count) {
-										players += '<span class="ui blue circular label">+' + (data.player_count - data.player_list.length) + '</span>';
-									}
+                  var promises = [];
+                  $.each(data.player_list, function (key, value) {
+                    var request = $.get(URLBuild('profile/' + value.name), function() {
+                        players += '<a href="' + URLBuild('profile/' + value.name) + '" data-tooltip="' + value.name + '" data-variation="mini" data-inverted="" data-position="bottom center"><img class="ui mini circular image" src="' + avatarSource.replace('{x}', value.id).replace('{y}', 64) + '" alt="' + value.name + '"></a>';
+                    }).fail(function() {
+                        players += '<a data-tooltip="' + value.name + '" data-variation="mini" data-inverted="" data-position="bottom center"><img class="ui mini circular image" src="' + avatarSource.replace('{x}', value.id).replace('{y}', 64) + '" alt="' + value.name + '"></a>';
+                      });
+                    promises.push(request);
+                  });
+                  
+                  $.when.apply($, promises).always(function () {
+                    if(data.player_list.length < data.player_count) {
+                      players += '<span class="ui blue circular label">+' + (data.player_count - data.player_list.length) + '</span>';
+                    }
+                    $(serverElem).find('#server-players').html(players);
+                  });
 
 								} else {
 									players += noPlayersOnline;
