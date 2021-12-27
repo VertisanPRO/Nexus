0c8f100aed53bdc3886a8072aa277549ead6e25b
itzJOHv <me@djoh.xyz>
2021-12-28 01:16:28 +0200

    Update widgets.php

--- modules/Nexus/language/Russian/widgets.php
+++ modules/Nexus/language/Russian/widgets.php
@@ -12,15 +12,15 @@
 
 $language = array(
 	// Donation Widget
-        'donate_widget_label' => 'Донат Виджет',
-        'donate_email_label' => 'Почта PayPal',
-        'first_amount_label' => 'Первая Сумма',
-        'second_amount_label' => 'Вторая Сумма',
-        'third_amount_label' => 'Третья Сумма',
+    'donate_widget_label' => 'Донат-виджет',
+    'donate_email_label' => 'Почта PayPal',
+    'first_amount_label' => 'Первая сумма',
+    'second_amount_label' => 'Вторая сумма',
+    'third_amount_label' => 'Третья сумма',
 
     // Message Widget
-        'message_widget_label' => 'Виджет Сообщения',
-        'message_title_label' => 'Название Виджета',
-        'message_text_label' => 'Текст Виджета',
-        'message_icon_label' => 'Картинка Виджета',
+    'message_widget_label' => 'Виджет сообщения',
+    'message_title_label' => 'Название виджета',
+    'message_text_label' => 'Текст виджета',
+    'message_icon_label' => 'Картинка виджета',
 );
