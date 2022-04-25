6f5bc5b57a2cff54b7fe57d02c43e5c3a8f6c0c8
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2022-04-25 12:55:42 +0300

    fix: discord id

--- custom/templates/Nexus/template_settings/frontend.php
+++ custom/templates/Nexus/template_settings/frontend.php
@@ -7,7 +7,7 @@
     if ($this->_cache->isCached('ds_status_ping')) {
         $discord_server = $this->_cache->retrieve('ds_status_ping');
     } else {
-        $discord_server = NexusUtill::getDsServer($settings_data['DISCORD_ID']);
+        $discord_server = NexusUtill::getDsServer($settings_data['discordID']);
     }
     $this->_cache->store('ds_status_ping', $discord_server, 60);
     $this->_smarty->assign(array(
