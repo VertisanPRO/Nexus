01e498f8b9493d05eaa5a3af1c0f3da98e18544d
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2022-06-10 19:27:48 +0300

    Remove WidgetPack (gigabait-official/widgetpack)

--- modules/WidgetPack/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once(ROOT_PATH . '/modules/WidgetPack/module.php');
-
-$widget_language = new Language(ROOT_PATH . '/modules/WidgetPack/language', LANGUAGE);
-$module = new WidgetPack_Module($language, $widget_language, $pages);
