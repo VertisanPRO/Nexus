3c9ab3c50854fb849c48c5c492c1feeca30642da
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2022-08-15 08:35:14 +0300

    Store new version in cache

--- custom/templates/Nexus/template_settings/upgrade.php
+++ custom/templates/Nexus/template_settings/upgrade.php
@@ -2,3 +2,7 @@
 
 # As of now this is the first release of upgrades, there's nothing
 # unlink('custom/templates/Nexus/...');
+
+$cache->setCache('update_check_nexus');
+$update_check_nexus = NexusUtil::updateCheckNexus();
+$cache->store('update_check_nexus', $update_check_nexus, 3600);
