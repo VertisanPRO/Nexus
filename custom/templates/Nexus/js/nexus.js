144b9e0007ec46cc788ddb192cb5e77e3c938612
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2022-08-27 14:37:38 +0300

    Update nexus.js

--- custom/templates/Nexus/js/nexus.js
+++ custom/templates/Nexus/js/nexus.js
@@ -4,17 +4,9 @@ $('.ui.sticky')
     });
 
 window.onload = function () {
-    document.body.classList.add('loaded');
-};
-
-window.onload = function () {
     document.body.classList.add('loaded_hiding');
     window.setTimeout(function () {
         document.body.classList.add('loaded');
         document.body.classList.remove('loaded_hiding');
     }, 500);
 };
-
-$('.ui.left.sidebar').sidebar({
-    transition: 'overlay'
-});
