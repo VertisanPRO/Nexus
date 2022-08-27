0243dd5e06af4f8347315b9ae840ef3fa7632fb5
Sylensky <admin@medieval-realm.net>
2022-08-28 01:19:00 +0200

    this is used on mobile/low resolutions

--- custom/templates/Nexus/js/nexus.js
+++ custom/templates/Nexus/js/nexus.js
@@ -3,10 +3,14 @@ $('.ui.sticky')
         context: '#context'
     });
 
-window.onload = function () {
+window.onload = function() {
     document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
+    window.setTimeout(function() {
         document.body.classList.add('loaded');
         document.body.classList.remove('loaded_hiding');
     }, 500);
 };
+
+$('.ui.left.sidebar').sidebar({
+    transition: 'overlay'
+});
